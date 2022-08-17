2b024fe623ea248908402763d00097d3e1131a4e
Tarcisio Leles <tarcisio_lp@hotmail.com>
2022-08-17 14:40:42 -0300

    cisco

--- projeto/Projeto/ciscoxml.bat
+++
@@ -1,5 +0,0 @@
-@echo off 
-xcopy "\\bancoob.br\NETLOGON\configuration.xml" "C:\ProgramData\Cisco\Cisco AnyConnect Secure Mobility Client\Network Access Manager\system\" /Y
-
-net stop nam
-net start nam
